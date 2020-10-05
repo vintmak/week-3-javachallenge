b2f72873b88163d3ded7398fd864c8249e9ea59e
vintmak <71264330+vintmak@users.noreply.github.com>
2020-10-05 19:50:38 +0800

    Update part2.js

--- part2.js
+++ part2.js
@@ -16,6 +16,7 @@ function largestSentence(sentence){
 console.log(largestSentence("The quick brown fox jumped over the lazy dog"))
 
 
+
 Q2 Largest Adjacent Product
 
 function largestAdjacent(numbers){
@@ -34,3 +35,36 @@ numbers[i+1];
 }
 
 console.log(largestAdjacent([3,6,-2,-5,7,3]))
+
+
+
+Q3 Transform hour to military format
+
+function transformMilitary(timeString){
+  if (timeString.charAt(8)== "A"){
+    if (timeString.substring(0,2) == "12"){
+      return "00"+timeString.substring(2,8);
+    }
+    else {
+      return timeString.substring(0,8)
+    }
+  }
+  else {
+    if (timeString.substring(0,2) == "12"){
+      return timeString.substring(0,8)
+    }
+  }
+    else {
+      if (timeString.substring(0,2) == "12"){
+        return timeString.substring(0,8) 
+      }
+      else {
+        return               (parseInt(timeString.substring(0,2))+12)+timeString.substring(2,8)
+    }
+  }
+}
+
+console.log(transformMilitary("07:45:15AM"))
+console.log(transformMilitary("07:45:15PM"))
+console.log(transformMilitary("12:45:15AM"))
+console.log(transformMilitary("12:45:15PM"))
